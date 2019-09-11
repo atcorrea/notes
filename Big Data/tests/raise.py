11884a9ae08d956f4841d3c6e60bab450a8ee8e8
Andre <andre.torres@capco.com>
2019-09-11 11:09:13 -0300

    updated raise.py script file

--- Big Data/tests/raise.py
+++ Big Data/tests/raise.py
@@ -3,13 +3,14 @@
 from pyspark import SparkConf, SparkContext
 conf = (SparkConf().setMaster('local').setAppName('app').set('spark.executor.memory', '1g'))
 sc = SparkContext(conf = conf)
+sc.setLogLevel("ERROR")
 
 data = sc.textFile('monthly_salary_brazil.csv').map(lambda x: x.split(','))
 
 header = data.first()
-rows = data.filter(lambda x: x != header).map(lambda y: [int(y[0]), y[1], y[2], float(y[3])])
+rows = data.filter(lambda x: x != header).map(lambda y: [int(y[0]), y[1].encode('latin-1'), y[2].encode('latin-1'), round(float(y[3]), 2)])
 
-raised = rows.map(lambda x: [x[0], x[1], x[2], x[3] * 1.1])
+raised = rows.map(lambda x: [x[0], x[1], x[2], round(x[3] * 1.1, 2)])
 
 
 before = rows.take(10)
